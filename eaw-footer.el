a07027dc9eaa0cdcf5b82840350e983fa0b75f96
Youhei SASAKI <uwabami@gfd-dennou.org>
2026-02-26 13:16:23 +0900

    Drop Emacs 22
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- eaw-footer.el
+++ eaw-footer.el
@@ -1,18 +1,15 @@
 
 ;;;###autoload
 (defun eaw-init ()
-  (when (= emacs-major-version 22)
-    (utf-translate-cjk-set-unicode-range code-wide))
-  (when (>= emacs-major-version 23)
-    (while (char-table-parent char-width-table)
-      (setq char-width-table (char-table-parent char-width-table)))
-    (let ((table (make-char-table nil)))
-      (mapc (lambda (range) (set-char-table-range table range 1))
-            code-half)
-      (mapc (lambda (range) (set-char-table-range table range 2))
-            code-wide)
-      (optimize-char-table table)
-      (set-char-table-parent table char-width-table)
-      (setq char-width-table table))))
+  (while (char-table-parent char-width-table)
+    (setq char-width-table (char-table-parent char-width-table)))
+  (let ((table (make-char-table nil)))
+    (mapc (lambda (range) (set-char-table-range table range 1))
+          code-half)
+    (mapc (lambda (range) (set-char-table-range table range 2))
+          code-wide)
+    (optimize-char-table table)
+    (set-char-table-parent table char-width-table)
+    (setq char-width-table table)))
 
 ;;; eaw.el ends here
